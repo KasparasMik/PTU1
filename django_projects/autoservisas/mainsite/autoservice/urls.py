f717ea427605a60bd01078c7cf087183f89f0c51
Kasparas Mikenas <kasparascyber@gmail.com>
2022-05-12 20:32:31 +0200

    django project update

--- django_projects/autoservisas/mainsite/autoservice/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('autoservice/', include('autoservice.urls')),
-    path('admin/', admin.site.urls),
-]
